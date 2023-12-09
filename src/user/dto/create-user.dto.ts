118bb23f349658247d648e5d45c3985107f38051
Solium Dev <lucas.develop.prog@gmail.com>
2023-12-09 14:05:13 -0300

    feat: CreateUserDto updated

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsStrongPassword,
+  IsStrongPasswordOptions,
+} from 'class-validator';
+
+const passwordDecoratorOptions: IsStrongPasswordOptions = {
+  minLength: 6,
+  minUppercase: 1,
+};
+
+export class CreateUserDto {
+  @IsStrongPassword(passwordDecoratorOptions) password: string;
+  @IsEmail() email: string;
+}
