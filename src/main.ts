cd47cc1363525ecad98b392c229c7e7580deb7ee
Solium Dev <lucas.develop.prog@gmail.com>
2023-12-11 16:29:34 -0300

    feat: NestJS logger added

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const port = 3000;
-  await app.listen(port);
-  console.log(`Server running at localhost:${port}`);
+  app.listen(port).then(() => {
+    const logger = new Logger(bootstrap.name);
+    logger.log('Server listening on ' + port);
+  });
 }
 bootstrap();
