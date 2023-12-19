bf31acfb5b72e7ebabceb118a715d8b27e861823
Solium Dev <lucas.develop.prog@gmail.com>
2023-12-18 22:49:36 -0300

    feat: Credentials DTO created and used in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CredentialsDto } from './dto/credentials.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @Post()
+  login(credentials: CredentialsDto) {
+    return this.authService.login(credentials);
+  }
 }
