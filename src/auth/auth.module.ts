531406063362c76fe3240a500b471d82f2d96dd7
Solium Dev <lucas.develop.prog@gmail.com>
2023-12-20 22:52:33 -0300

    fix: JwtModule wrongly registered

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: process.env.JWT_EXP },
-    }),
-  ],
+  imports: [JwtModule],
   controllers: [AuthController],
   providers: [AuthService, PrismaService],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthorizationGuard } from '../common/guards/authorization.guard';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, AuthorizationGuard],
 })
 export class UserModule {}
