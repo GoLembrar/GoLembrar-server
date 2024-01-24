00a8dd0dbd6f3269120855a95095ecd406de1a7f
MatheusVict <matheusvictorhenrique@gmail.com>
2024-01-23 21:43:14 -0300

    chore: add swagger docs on auth controller
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CredentialsDto } from './dto/credentials.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { OkResponse } from '../swagger/decorators/ok.decorator';
+import { OkResponseModel } from './swagger/okResponseModel.swagger';
+import { Unauthorized } from '../swagger/decorators/unauthorized.decorators';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post()
+  @ApiOperation({ summary: 'Login' })
+  @OkResponse(OkResponseModel)
+  @Unauthorized()
   login(@Body() credentials: CredentialsDto) {
     return this.authService.login(credentials);
   }
