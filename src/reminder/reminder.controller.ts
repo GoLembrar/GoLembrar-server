1223fd87ceaf7e04b4bec62c36c6c376a6df9be6
MatheusVict <matheusvictorhenrique@gmail.com>
2024-01-23 21:18:10 -0300

    chore: reminder swagger custom response decorators
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/reminder/reminder.controller.ts
+++ src/reminder/reminder.controller.ts
@@ -4,13 +4,19 @@ import { CreateReminderDto } from './dto/create-reminder.dto';
 import { AuthorizationGuard } from '../common/guards/authorization.guard';
 import { AddOwnerToBodyGuard } from './guards';
 import { ApiTags } from '@nestjs/swagger';
+import { OkResponse } from '../swagger/decorators/ok.decorator';
+import { createReminderResponse } from './swagger/createReminderResponse.swagger';
+import { Unauthorized } from '../swagger/decorators/unauthorized.decorators';
 
 @Controller('reminder')
 @ApiTags('reminder')
 export class ReminderController {
   constructor(private readonly reminderService: ReminderService) {}
+
   @UseGuards(AuthorizationGuard, AddOwnerToBodyGuard)
   @Post('')
+  @OkResponse(createReminderResponse)
+  @Unauthorized()
   createReminder(@Body() createReminderDto: CreateReminderDto) {
     return this.reminderService.createReminder(createReminderDto);
   }
