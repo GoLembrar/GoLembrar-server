70cd1530ac51b123dd4ba1be6bc96678ba6072e2
MatheusVict <matheusvictorhenrique@gmail.com>
2024-01-23 21:13:54 -0300

    feat: add response with custom decorators

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -3,35 +3,55 @@ import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { AuthorizationGuard } from '../common/guards/authorization.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Unauthorized } from '../swagger/decorators/unauthorized.decorators';
+import { CreateCategoryResponse } from './swagger/createCategoryResponse.swagger';
+import { NotFoundResponse } from '../swagger/decorators/notFound.decorator';
+import { OkResponse } from '../swagger/decorators/ok.decorator';
+import { OkResponseModel } from './swagger/okResponseModel.swagger';
 
 @Controller('category')
 @UseGuards(AuthorizationGuard)
+@ApiTags('category')
+@ApiBearerAuth()
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+  constructor(private readonly categoryService: CategoryService) { }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoryService.create(createCategoryDto);
+  @CreateCategoryResponse()
+  @Unauthorized()
+  async create(@Body() createCategoryDto: CreateCategoryDto) {
+    return await this.categoryService.create(createCategoryDto);
   }
 
   @Get()
-  findAll() {
-    return this.categoryService.findAll();
+  @OkResponse([OkResponseModel])
+  @Unauthorized()
+  async findAll() {
+    return await this.categoryService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoryService.findOne(+id);
+  @OkResponse(OkResponseModel)
+  @Unauthorized()
+  @NotFoundResponse()
+  async findOne(@Param('id') id: string) {
+    return await this.categoryService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoryService.update(+id, updateCategoryDto);
+  @OkResponse(OkResponseModel)
+  @Unauthorized()
+  @NotFoundResponse()
+  async update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
+    return await this.categoryService.update(+id, updateCategoryDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoryService.remove(+id);
+  @Unauthorized()
+  @NotFoundResponse()
+  async remove(@Param('id') id: string) {
+    await this.categoryService.remove(+id);
   }
 }
---
+++ src/category/swagger/createCategoryResponse.swagger.ts
@@ -0,0 +1,11 @@
+import { applyDecorators } from "@nestjs/common";
+import { ApiResponse } from "@nestjs/swagger";
+
+export function CreateCategoryResponse() {
+    return applyDecorators(
+        ApiResponse({
+            status: 201,
+            description: 'category created',
+          }),
+    );
+}
