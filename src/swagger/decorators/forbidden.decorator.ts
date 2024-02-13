140cf9d4af97f2bb7a5a087a2314cd64d04227f3
Lucas Nathan Henrique da Silva <lucas.develop.prog@gmail.com>
2024-02-13 19:34:33 -0300

    refactor: rename files with 'Response' sufix

--- src/swagger/decorators/forbidden.decorator.ts
+++ src/swagger/decorators/forbidden.decorator.ts
@@ -2,7 +2,7 @@ import { applyDecorators } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
 import { ForbiddenException } from '../../common/utils/exception/forbidden.exception';
 
-export function Forbidden() {
+export function ForbiddenResponse() {
   return applyDecorators(
     ApiResponse({
       status: 403,
