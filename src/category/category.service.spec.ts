b582f8d002a99aebdf2ba85d272fd44093968dbc
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-08 20:28:09 -0300

    implementa tests

--- src/category/category.service.spec.ts
+++ src/category/category.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('CategoryService', () => {
   let service: CategoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [
+        CategoryService,
+        {
+          provide: PrismaService,
+          useValue: {
+            // Defina os métodos mock do PrismaService que o ReminderService usa
+          },
+        },
+      ],
     }).compile();
 
     service = module.get(CategoryService);
