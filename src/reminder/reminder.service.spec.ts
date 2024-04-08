681e695e4fb36934c68ccf989f8de18aeef011e6
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-08 20:29:03 -0300

    implementa testes

--- src/reminder/reminder.service.spec.ts
+++ src/reminder/reminder.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReminderService } from './reminder.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('ReminderService', () => {
   let service: ReminderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReminderService],
+      providers: [
+        ReminderService,
+        {
+          provide: PrismaService,
+          useValue: {
+            // Defina os métodos mock do PrismaService que o ReminderService usa
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ReminderService>(ReminderService);
