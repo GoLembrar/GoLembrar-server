92c97eb84f6370ddb6884c83501d06c347dd505d
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-25 12:50:49 -0300

    adiciona novo modulo 'contact' para implementar logica e recursos necessarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { ReminderModule } from './reminder/reminder.module';
 import { CategoryModule } from './category/category.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { CategoryModule } from './category/category.module';
     AuthModule,
     ReminderModule,
     CategoryModule,
+    ContactModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
