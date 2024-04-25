83792bfaa84d4502e2c1eb681befcaacc6f25f84
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-25 19:46:58 -0300

    DTO para criação de contact

--- src/contact/dto/create-contact.dto.ts
+++ src/contact/dto/create-contact.dto.ts
@@ -1,1 +1,40 @@
-export class CreateContactDto {}
+/* 
+model Contact {
+  id       Int       @id @default(autoincrement())
+  name     String    @db.VarChar(128)
+  identify String    @db.VarChar(64)
+  platform Platforms
+  user     Users     @relation(fields: [usersId], references: [id])
+  usersId  Int
+
+  @@index([name, identify])
+  @@map("contacts")
+}
+
+enum Platforms {
+  WHATSAPP
+  TELEGRAM
+  EMAIL
+  DISCORD
+}
+*/
+
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateContactDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  identify: string;
+
+  @IsNotEmpty()
+  @IsString()
+  platform: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  userId: number;
+}
