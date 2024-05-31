07914b55efcfd0e0f0b7880670aefe8a4ed016cc
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-31 18:36:40 -0300

    remove importação não usadas

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { ReminderModule } from './reminder/reminder.module';
 import { CategoryModule } from './category/category.module';
-import { ContactModule } from './contact/contact.module';
-import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
