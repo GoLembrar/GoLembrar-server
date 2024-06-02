90b5f494fa7a6c6eafa1721e1367903b8b8475f7
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-06-02 17:40:20 -0300

    adiciona mais informações no payload do jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -29,8 +29,12 @@ export class AuthService {
 
     if (!isPasswordMatching)
       throw new UnauthorizedException('Credenciais inválidas');
+
     const jwtPayloadData: JwtPayload = {
       id: foundUser.id,
+      email: foundUser.email,
+      name: foundUser.name,
+      phone: foundUser.phone,
     };
 
     const token = this.jwt.sign(jwtPayloadData);
