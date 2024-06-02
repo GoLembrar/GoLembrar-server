bf6cf3cf3159f294aff5c1253ed251ce78eeef72
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-06-02 17:41:15 -0300

    remoção do campo password

--- src/auth/dto/credentials.dto.ts
+++ src/auth/dto/credentials.dto.ts
@@ -1,22 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
-  IsStrongPassword,
-  IsStrongPasswordOptions,
+  IsNotEmpty
 } from 'class-validator';
 
-const passwordDecoratorOptions: IsStrongPasswordOptions = {
+/* const passwordDecoratorOptions: IsStrongPasswordOptions = {
   minLength: 6,
   minNumbers: 0,
   minSymbols: 0,
   minUppercase: 1,
-};
+}; */
 
 export class CredentialsDto {
   @ApiProperty({ example: 'user@email.com' })
   @IsEmail()
+  @IsNotEmpty()
   email: string;
+
   @ApiProperty({ example: '123456' })
-  @IsStrongPassword(passwordDecoratorOptions)
+  @IsNotEmpty()
   password: string;
 }
