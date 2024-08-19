5b494adfb7e16e6365496559bd210eadaba3606d
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-08-19 08:32:02 -0300

    refactor: add description in OkResponse swagger decorator and transfer the reminder condition from controller to service in getReminderById method, include descrition in Okresponse swagger decorator and remove NotFoundResponse swagger decorator in getRemindersByUser method, add description and message in CreatedResponse swagger decorator and change void return promise to Response including HttpStatus created and message returns in createReminder method, and include NoContentResponse swagger decorator and remove message returns in updateReminder method

--- src/reminder/reminder.controller.ts
+++ src/reminder/reminder.controller.ts
@@ -3,7 +3,6 @@ import {
   Controller,
   Get,
   HttpStatus,
-  NotFoundException,
   Param,
   Patch,
   Post,
@@ -12,12 +11,11 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { Response } from 'express';
+import { response, Response } from 'express';
 import { AccessTokenGuard } from '../auth/guards/access-token/access-token.guard';
 import { RequestWithUser } from '../common/utils/types/RequestWithUser';
 import { CreatedResponse } from '../swagger/decorators/created.decorator';
 import { ForbiddenResponse } from '../swagger/decorators/forbidden.decorator';
-import { NotFoundResponse } from '../swagger/decorators/notFound.decorator';
 import { OkResponse } from '../swagger/decorators/ok.decorator';
 import { UnauthorizedResponse } from '../swagger/decorators/unauthorized.decorator';
 import { CreateReminderDto } from './dto/create-reminder.dto';
@@ -25,6 +23,8 @@ import { UpdateReminderDto } from './dto/update-reminder.dto';
 import { AddOwnerToBodyGuard } from './guards';
 import { ReminderService } from './reminder.service';
 import { GetReminderResponse } from './swagger/getReminderResponse.swagger';
+import { NotFoundResponse } from '../swagger/decorators/not-found.decorator';
+import { NoContentResponse } from '../swagger/decorators/no-content.decorator';
 
 @Controller('reminder')
 @ApiTags('reminder')
@@ -35,21 +35,17 @@ export class ReminderController {
 
   @Get(':id')
   @ApiOperation({ summary: 'Get a reminder by id' })
-  @OkResponse(GetReminderResponse)
+  @OkResponse('Reminder found response successfully', GetReminderResponse)
   @UnauthorizedResponse()
   @NotFoundResponse()
   async getReminderById(@Param('id') id: string) {
-    const reminder = await this.reminderService.getReminderById(id);
-    if (!reminder)
-      throw new NotFoundException('Não foi possível encontrar o lembrete');
-    return reminder;
+    return await this.reminderService.getReminderById(id);
   }
 
   @Get('')
   @ApiOperation({ summary: 'Get all reminders from user' })
-  @OkResponse([GetReminderResponse])
+  @OkResponse('Reminders found response successfully', [GetReminderResponse])
   @UnauthorizedResponse()
-  @NotFoundResponse()
   async getRemindersByUser(@Req() request: RequestWithUser) {
     const userId: string = request.user['sub'];
     return this.reminderService.getUserReminders(userId);
@@ -58,20 +54,23 @@ export class ReminderController {
   @Post('')
   @UseGuards(AddOwnerToBodyGuard)
   @ApiOperation({ summary: 'Create a new reminder.' })
+  @CreatedResponse('Reminder created response successfully', 'reminder created')
   @UnauthorizedResponse()
-  @CreatedResponse()
   async createReminder(
     @Req() request: RequestWithUser,
     @Body() createReminderDto: CreateReminderDto,
-  ): Promise<void> {
+  ): Promise<Response> {
     console.log(request.user['sub']);
     createReminderDto.ownerId = request.user['sub'];
-    return this.reminderService.createReminder(createReminderDto);
+    await this.reminderService.createReminder(createReminderDto);
+    return response.status(HttpStatus.CREATED).json({
+      message: 'reminder created',
+    });
   }
 
   @Patch(':id')
   @ApiOperation({ summary: 'Update one reminder.' })
-  @OkResponse()
+  @NoContentResponse('Reminder updated response successfully')
   @UnauthorizedResponse()
   @ForbiddenResponse()
   async updateReminder(
@@ -80,8 +79,6 @@ export class ReminderController {
     @Res() response: Response,
   ): Promise<Response> {
     await this.reminderService.updateReminder(id, updateReminderDto);
-    return response.status(HttpStatus.NO_CONTENT).json({
-      message: 'reminder updated',
-    });
+    return response.status(HttpStatus.NO_CONTENT);
   }
 }
