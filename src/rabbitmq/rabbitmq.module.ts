7d4278dca96dccc103b03848a7b9c030107c8053
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-09-11 15:45:07 -0300

    refactor: remove QueueServicesList provide and all imports

--- src/rabbitmq/rabbitmq.module.ts
+++ src/rabbitmq/rabbitmq.module.ts
@@ -1,32 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ClientProxyFactory, Transport } from '@nestjs/microservices';
-import { QueueList } from '../queue/utils/queue-list';
 import { RabbitMQService } from './rabbitmq.service';
-import { QueueServicesList } from '../queue/utils/queue-services-list';
-import { EmailQueueModule } from '../queue/email-queue/email-queue.module';
-import { EmailListenerModule } from '../events/email-listener/email-listener.module';
+
+// import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+// import { QueueList } from '../queue/utils/queue-list';
+// import { QueueServicesList } from '../queue/utils/queue-services-list';
+// import { EmailQueueModule } from '../queue/email-queue/email-queue.module';
+// import { EmailListenerModule } from '../events/email-listener/email-listener.module';
 
 @Module({
-  providers: [
-    RabbitMQService,
-    {
-      provide: QueueServicesList.RABBITMQ_SERVICE,
-      useFactory: () => {
-        console.log('CONFIGURANDO RABBITMQ');
-        return ClientProxyFactory.create({
-          transport: Transport.RMQ,
-          options: {
-            urls: [process.env.RABBITMQ_URL],
-            queue: QueueList.DEFAULT,
-            queueOptions: {
-              durable: false,
-            },
-          },
-        });
-      },
-    },
-  ],
-  imports: [EmailQueueModule, EmailListenerModule],
+  providers: [RabbitMQService],
   exports: [RabbitMQService],
 })
 export class RabbitmqModule {}
